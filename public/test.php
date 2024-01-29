340951e83eb4998c283296f228cde0d699ea6226
fabien-design <fabienrozier60@gmail.com>
2024-01-29 14:06:05 +0100

    Getimage for each car

--- public/test.php
+++ public/test.php
@@ -5,16 +5,12 @@ use App\Controller\HomeController;
 
 // Instancier le HomeController
 $homeController = new HomeController();
-
-
 $array = json_decode(file_get_contents("../cars.json"), true);
-shuffle($array);
-$array = array_splice($array, 0 , 10);
-
-foreach ($array as $car) {
+foreach ($array as &$car) {
     $query = $car['Name'] . " " . $car['Year'];
     // Appeler la fonction searchImg
     $response = $homeController->searchImg($query);
+
     if ($response != null) {
         // Créer un objet DOMDocument
         $dom = new DOMDocument;
@@ -31,13 +27,20 @@ foreach ($array as $car) {
         $images = $xpath->query('/html/body/div[3]//img');
 
         if ($images->length > 0) {
-            $firstImageSrc = $images->item(0)->getAttribute('src'); # ca fonctionne niquel
+            $firstImageSrc = $images->item(0)->getAttribute('src');
             echo "<img src=" . $firstImageSrc . ">";
-        } else {
-            echo 'Aucune balise <img> trouvée dans /html/body/div[3].';
-        }
 
+            // Ajouter le lien de l'image au tableau de la voiture
+            $car['photo'] = $firstImageSrc;
+        } else {
+            echo "aucune image";
+            $car['photo'] = null;
+        }
     } else {
         echo "aucun resultat";
+        $car['photo'] = null;
     }
 }
+
+// Mettre à jour le fichier JSON avec les nouvelles informations
+file_put_contents("../cars.json", json_encode($array, JSON_PRETTY_PRINT));
